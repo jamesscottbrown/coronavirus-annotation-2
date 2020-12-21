bac587a956d5d6d934287ae26c1664ec913015d4
Ubuntu <ubuntu@ip-172-31-33-173.us-east-2.compute.internal>
2020-12-21 19:14:03 +0000

    Add .env and use the env var in run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
     from app import app
-    app.run(debug=True)
+    app.run(debug=True, host=os.getenv("FLASK_HOST"))
