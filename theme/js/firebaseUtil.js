c89cb613ff53f85e351e766feaf20223ce2af125
Jen Rogers <jen@sci.utah.edu>
2020-12-21 22:04:30 -0700

    change last files from test branch

--- theme/js/firebaseUtil.js
+++ theme/js/firebaseUtil.js
@@ -1,65 +1,46 @@
 import firebase from 'firebase/app';
-import { currentUser, dataKeeper } from './dataManager';
 require('firebase/auth');
 require('firebase/database');
-import * as d3 from 'd3';
+import { currentUser, dataKeeper } from './dataManager';
+import { fbConfig } from '.';
+import { updateCommentSidebar } from './annotationDashboard/commentBar';
+
 
 export async function checkUser(callbackArray){
 
-  let fbConfig = await d3.json("../static/assets/firebase_data.json");
+  if (!firebase.apps.length) { 
+    firebase.initializeApp(fbConfig[0]);
+  }
 
-  console.log(fbConfig[0]);
-
-    if (!firebase.apps.length) {
-        firebase.initializeApp(fbConfig[0]);
-    }
-
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          currentUser.push(user);
-          callbackArray.forEach(fun=> {
-            console.log("is this working")
-            fun(user)});
-          // User is signed in.
-        } else {
-          console.log("NO USER", user);
-          // No user is signed in.
-        }
-    
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      currentUser.push(user);
+      callbackArray.forEach(fun=> {
+        fun(user);
       });
-      return currentUser;
-}
-
-export function pullDataFromDatabase(){
-
-  let ref = firebase.database().ref();  
-  ref.on("value", function(snapshot) {
-      dataKeeper.push(snapshot.val());
-  }, function (error) {
-      console.log("Error: " + error.code);
+      checkDatabase([updateCommentSidebar])
+          // User is signed in.
+    } else {
+      console.log("NO USER", user);
+          // No user is signed in.
+    }
+    
   });
-
-  return dataKeeper;
+  return currentUser;
 }
-// export function checkDatabase(ref, callback, extraArgs){
- 
-//   ref.on("value", function(snapshot) {
 
-//       extraArgs != null ? callback(snapshot.val(), extraArgs) : callback(snapshot.val());
-//       dataKeeper.push(snapshot.val());
-      
-//   }, function (error) {
-//       console.log("Error: " + error.code);
-//   });
-
-// }
-
-export function checkDatabase(ref){
- 
+export function checkDatabase(callbackArray){
+  let ref = firebase.database().ref(); 
   ref.on("value", function(snapshot) {
 
       //extraArgs != null ? callback(snapshot.val(), extraArgs) : callback(snapshot.val());
       dataKeeper.push(snapshot.val());
+
+      console.log('snapshooot',snapshot.val());
+
+      callbackArray.forEach(fun=> {
+        fun(snapshot.val());
+      });
       
   }, function (error) {
       console.log("Error: " + error.code);
