ddd117e8732a1e582939972b15a658199e26e6f1
Jen Rogers <jen@sci.utah.edu>
2020-12-30 10:01:39 -0700

    fix python lint errors

--- app/index/views.py
+++ app/index/views.py
@@ -1,39 +1,24 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 from flask import (
-    Blueprint, 
-    render_template, 
-    request,
-    url_for,
-    session,
-    redirect
+    Blueprint,
+    render_template,
 )
-
 import time
-#pytfrom firebase_config import fdb, fauth
 import os
+
 
 mod = Blueprint('index', __name__)
 
-def format_server_time():
-  server_time = time.localtime()
-  return time.strftime("%I:%M:%S %p", server_time)
 
-def newuser():
-    return redirect(url_for('dashboard.index'))
+def format_server_time():
+    server_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", server_time)
+
 
 @mod.route('/')
 def index():
-    return render_template('/home/index.html', apiKey= os.getenv("FIREBASE_API_KEY"))
-
-@mod.route('/login')
-def login():
-    return render_template('/login/index.html')
-
-@mod.route('/newuser')
-def newuser():
-    return render_template('newuser/index.html')
-
-
-
-
+    return render_template(
+        '/home/index.html',
+        apiKey=os.getenv("FIREBASE_API_KEY")
+        )
